77afab0b5d67a22a57044e90376ad9fc8a663bcd
brijesh1130 <105264795+brijesh1130@users.noreply.github.com>
2023-10-09 12:16:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# FoodHunt
+
+A food delivery application made using React.
+
+## Tech Stack
+
+- React
+- Parcel
+- Tailwind CSS
+- Redux-toolkit
+
+## Features
+
+- Get restaurants by current location
+- Add to cart
+- Shimmer UI
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      Foody, made by{' '}
+      <a
+        href='www.linkedin.com/in/brijesh-kanani-b55205229'
+        target={'_blank'}
+        className='text-orange-500'
+      >
+       Brijesh Kanani
+      </a>
+    </div>
+  );
+};
+export default Footer;
